49b87415c0cb1d55f41138fe05c8ef93e9a0688e
simon chege <petekim211@gmail.com>
2020-03-27 17:06:19 +0300

    footer finishing

--- app/route.py
+++ app/route.py
@@ -5,3 +5,11 @@ from flask import render_template,url_for
 @app.route('/index')
 def index():
     return render_template("index.html", title='Home')
+
+@app.route('/myvehicles')
+def myVehicles():
+    return render_template("myvehicles.html", title='My Vehicles')
+
+@app.route('/user/<id>')
+def usercenter(id):
+    return render_template("usercenter.html", title='User Account')
